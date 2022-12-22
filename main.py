3986e85a2d584b6ed582f57c2327d5a67faa8fb7
Camilo <campero189@gmail.com>
2022-12-22 21:12:34 +0100

    Project completion and data added

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import plotly.express as px
 
 # st.title("Hello World")
 # st.markdown("## My first streamlit dashboard!")
 
-DATA_URL = ("C:\\Users\\cromero\\Dropbox\\Learning\\Build_a_data_app_streamlit\\Motor_Vehicle_Collisions_-_Crashes.csv")
+DATA_URL = (".\\Motor_Vehicle_Collisions_-_Crashes.csv")
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used"
@@ -35,6 +36,8 @@ def load_data(nrows):
 # longitude
 
 data = load_data(100000)
+original_data = data
+
 st.header("Where are the most people injured in NYC")
 injured_people = st.slider("Number of persons injured in vehicle collisions", 0, 19)
 # to filter data in pandas
@@ -51,9 +54,56 @@ st.markdown("Vehicle collisions between %i:00 and %i:00" % (hour, (hour + 1) % 2
 midpoint = (np.average(data['latitude']), np.average(data['longitude']))
 st.write(pdk.Deck(
     map_style="mapbox://styles/mapbox/light-v9",
-    initial_view_state={"latitude": midpoint[0], "longitude": midpoint[1], "zoom": 11, "pitch": 50}
+    initial_view_state={
+        "latitude": midpoint[0],
+        "longitude": midpoint[1],
+        "zoom": 11,
+        "pitch": 50
+    },
+    # Adding layers to the map
+    layers=[
+        pdk.Layer(
+            "HexagonLayer",
+            data=data[['date/time', 'latitude', 'longitude']],
+            get_position=['longitude', 'latitude'],
+            radius=100,
+            extruded=True,
+            pickable=True,
+            elevation_scale=4,
+            elevation_range=[0, 1000]
+
+        )
+    ],
 ))
 
+## Making histograms
+st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+filtered = data[
+    (data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour + 1))
+    ]
+hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0, 60))[0]
+chart_data = pd.DataFrame({'minute': range(60), 'crashes': hist})
+fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
+st.write(fig)
+
+
+##
+st.header("Top 5 dangerous streets by affected type")
+select = st.selectbox('Affected type of people', ['Pedestrians', 'Cyclists', 'Motorists'])
+
+# Query by type of people and selecting the range.
+if select == "Pedestrians":
+    st.write(original_data.query("injured_pedestrians >= 1")[
+                 ["on_street_name", "injured_pedestrians"]]
+             .sort_values(by=['injured_pedestrians'], ascending=False).dropna(how='any')[:5])
+elif select == "Cyclists":
+    st.write(original_data.query("injured_cyclists >= 1")[
+                 ["on_street_name", "injured_cyclists"]]
+             .sort_values(by=['injured_cyclists'], ascending=False).dropna(how='any')[:5])
+else:
+    st.write(original_data.query("injured_motorists >= 1")[
+                 ["on_street_name", "injured_motorists"]]
+             .sort_values(by=['injured_motorists'], ascending=False).dropna(how='any')[:5])
 
 # Creating a checkbox that when checked will show the raw data.
 if st.checkbox("Show Raw Data", False):
